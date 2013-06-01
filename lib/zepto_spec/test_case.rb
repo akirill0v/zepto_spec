9b055f68af167024ff96639ba15e95594dcba5b0
Kirillov Alexander <saratovsource@gmail.com>
2013-06-01 12:25:50 +0400

    Mail test cases

---
+++ lib/zepto_spec/test_case.rb
@@ -0,0 +1,30 @@
+module ZeptoSpec
+  class TestCase
+
+    def assert(expression)
+      expression
+    end
+
+    class << self
+      def run
+        test_methods = instance_methods(false)
+        test_methods.each do |m|
+          run_test_method(m.to_s)
+        end
+      end
+
+      def run_test_method(method_name)
+        begin
+          result = self.new.instance_eval(method_name)
+          puts "#{method_name} - #{result ? 'PASSED' : 'ERROR'}"
+        rescue Exception => e
+          puts e.message
+          puts e.backtrace.inspect
+          puts "#{method_name} - FAIL"
+        ensure
+          # always executed
+        end
+      end
+    end
+  end
+end
